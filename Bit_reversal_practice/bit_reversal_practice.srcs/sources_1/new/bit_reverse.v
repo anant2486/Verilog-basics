a028af6bcd2f11d9ad39eb83d83a472d941c9f5e
anant2486 <anantsharma0920@gmail.com>
2026-07-25 12:36:52 +0530

    Added Bit_reversal_practice project

---
+++ Bit_reversal_practice/bit_reversal_practice.srcs/sources_1/new/bit_reverse.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.09.2025 16:59:11
+// Design Name: 
+// Module Name: bit_reverse
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module bit_reverse(
+
+    );
+endmodule
